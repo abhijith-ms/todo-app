77fa44bbc131e6797e6de53c404732e2540e4692
abhijith-ms <abhijithms.9526@gmail.com>
2025-01-24 12:57:50 +0530

    Switch to ES Modules for server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import jsonServer from "json-server"; 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+import path from "path";
+import express from "express";
+import { createRequire } from "module"; 
+const require = createRequire(import.meta.url); 
+const jsonServer = require("json-server"); 
+const app = express();
 const port = process.env.PORT || 3001;
 
-server.use(middlewares);
-server.use(router);
+app.use(express.static(path.join(process.cwd(), "dist")));
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+app.use("/api", middlewares); 
+app.use("/api", router);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
