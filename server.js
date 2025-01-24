16d63259d934d0048b02a99fc3c8e3b516f803cd
abhijith-ms <abhijithms.9526@gmail.com>
2025-01-24 11:59:00 +0530

    updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; 
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
